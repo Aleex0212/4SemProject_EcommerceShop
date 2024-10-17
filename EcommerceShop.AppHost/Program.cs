edf04ef84110d65f90405aa47a23f4442b370600
Aleex0212 <130439482+Aleex0212@users.noreply.github.com>
2024-10-17 17:14:57 +0200

    Add SagaOrchestrator

--- EcommerceShop.AppHost/Program.cs
+++ EcommerceShop.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.EcommerceShop_Blazor>("ecommerceshop-blazor");
 
+builder.AddProject<Projects.SagaOrchestrator>("sagaorchestrator");
+
 builder.Build().Run();
