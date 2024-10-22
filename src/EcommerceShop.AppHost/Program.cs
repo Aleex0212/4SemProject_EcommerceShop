9362cc18c1e9c190fbbbf3ea302a212d6663f90a
Aleex0212 <130439482+Aleex0212@users.noreply.github.com>
2024-10-22 13:10:22 +0200

    Opsætning af Aspire.AppHost

--- src/EcommerceShop.AppHost/Program.cs
+++ src/EcommerceShop.AppHost/Program.cs
@@ -1,7 +1,42 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.SagaOrchestrator>("sagaorchestrator");
+//  PubSub Channels 
+var daprStateStore = builder.AddDaprStateStore("daprStateStore");
+var CustomerChannel = builder.AddDaprPubSub("CustomerChannel");
+var OrderChannel = builder.AddDaprPubSub("OrderChannel");
+var ProductChannel = builder.AddDaprPubSub("ProductChannel");
+var CartChannel = builder.AddDaprPubSub("CartChannel");
 
-//builder.AddProject<Projects.EcommerceShop_FrontEnd>("ecommerceshop-frontend");
+// Project References
+builder
+    .AddProject<OrderService_Api>("OrderService")
+    .WithDaprSidecar("order-service")
+    .WithReference(daprStateStore)
+    .WithReference(CustomerChannel)
+    .WithReference(OrderChannel)
+    .WithReference(ProductChannel)
+    .WithReference(CartChannel);
 
-builder.Build().Run();
+builder
+    .AddProject<ProductService_Api>("ProductService")
+    .WithDaprSidecar("product-service")
+    .WithReference(ProductChannel)
+    .WithReference(OrderChannel);
+
+builder
+    .AddProject<CustomerService_Api>("CustomerService")
+    .WithDaprSidecar("customer-service")
+    .WithReference(CustomerChannel)
+    .WithReference(ProductChannel);
+
+builder
+    .AddProject<CartService_Api>("CartService")
+    .WithDaprSidecar("cart-service");
+
+string? daprPath = Environment.GetEnvironmentVariable("DAPR_PATH", EnvironmentVariableTarget.User);
+builder.AddDapr(opts => opts.DaprPath = daprPath ?? "default/dapr/path");
+
+var app = builder.Build();
+app.Run();
