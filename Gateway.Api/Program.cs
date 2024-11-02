e35d8835b2c19e504ae2ad3ee9b53a55ddf0f9b1
CHRISTOFFER\chris <chsk52994@edu.ucl.dk>
2024-11-02 17:05:45 +0100

    gateway api har hul igennem

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -1,8 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDaprClient();
@@ -15,8 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCloudEvents();
+
 //app.UseHttpsRedirection();
 
-app.Run();
+app.UseAuthorization();
+app.UseCloudEvents();
+app.MapControllers();
 
+app.Run();
