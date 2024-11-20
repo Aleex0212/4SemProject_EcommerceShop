79efbb4d2c43b7ab0b45d40e9f6305581ef42115
Aleex0212 <130439482+Aleex0212@users.noreply.github.com>
2024-11-20 14:49:25 +0100

    Add Blazor

---
+++ Ecommerce.FrontEnd/Interfaces/Refit/IAuthApi.cs
@@ -0,0 +1,12 @@
+﻿using global::Refit;
+using EcommerceShop.Common.Dto;
+using EcommerceShop.Common.Routes;
+
+namespace Ecommerce.FrontEnd.Interfaces.Refit
+{
+  public interface IAuthApi
+  {
+    [Post(Routes.GatewayRoutes.AuthRoutes.Login)]
+    Task<string> Login([Body] LoginDto loginDto);
+  }
+}
---
+++ Ecommerce.FrontEnd/Program.cs
@@ -0,0 +1,26 @@
+using Ecommerce.FrontEnd;
+using Ecommerce.FrontEnd.Interfaces.Refit;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+using Refit;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+#region Refit Interface Registration
+
+builder.Services.AddRefitClient<IAuthApi>()
+  .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
+
+#endregion
+
+#region Radzen Components
+builder.Services.AddScoped<NotificationService>();
+#endregion
+
+await builder.Build().RunAsync();
