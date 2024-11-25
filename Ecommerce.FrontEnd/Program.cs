e117ea303c363d691a7fa260ecc0310266746d98
Aleex0212 <130439482+Aleex0212@users.noreply.github.com>
2024-11-25 12:49:34 +0100

    Refit serialize opt. program.cs

--- Ecommerce.FrontEnd/Program.cs
+++ Ecommerce.FrontEnd/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 using Refit;
+using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,25 +14,29 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-#region ServiceRegistration
-builder.Services.AddBlazoredSessionStorage();
-#endregion
-
 #region Refit Interface Registration
-
-builder.Services.AddRefitClient<IAuthGatewayApi>()
+var serializer = SystemTextJsonContentSerializer.GetDefaultJsonSerializerOptions();
+serializer.Converters.Remove(serializer.Converters.Single(x => x.GetType().Equals(typeof(JsonStringEnumConverter))));
+var refitSettings = new RefitSettings
+{
+  ContentSerializer = new SystemTextJsonContentSerializer(serializer)
+};
+builder.Services.AddRefitClient<IAuthGatewayApi>(refitSettings)
   .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
 
 builder.Services
-  .AddRefitClient<IProductGatewayApi>()
+  .AddRefitClient<IProductGatewayApi>(refitSettings)
   .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
 
-  builder.Services.AddRefitClient<IOrderGatewayApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
+builder.Services.AddRefitClient<IOrderGatewayApi>(refitSettings)
+  .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
 
-  builder.Services.AddRefitClient<IUserGatewayApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
+builder.Services.AddRefitClient<IUserGatewayApi>(refitSettings)
+  .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7136/"));
+#endregion
 
+#region ServiceRegistration
+builder.Services.AddBlazoredSessionStorage();
 #endregion
 
 #region Radzen Components
