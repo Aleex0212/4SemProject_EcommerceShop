3bd4dafef1d1c3cee74bf508ae8741acca71097c
Alex <Alexander.c.Larsen@gmail.com>
2024-12-02 01:51:02 +0100

    Blazor

--- Ecommerce.FrontEnd/Refit/IProductGatewayApi.cs
+++ Ecommerce.FrontEnd/Refit/IProductGatewayApi.cs
@@ -8,5 +8,8 @@ namespace Ecommerce.FrontEnd.Refit
     {
         [Get(Routes.GatewayRoutes.ProductGatewayRoutes.Product)]
         Task<IEnumerable<ProductDto>?> GetProductsAsync([Header("Authorization")] string bearerToken);
+
+        [Post(Routes.GatewayRoutes.ProductGatewayRoutes.Product)]
+        Task<string> CreateProductAsync([Body] ProductDto productDto, [Header("Authorization")] string bearerToken);
     }
 }
